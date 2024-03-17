b37ee3bac95e6f1139040574613bcbd6f468b8f0
Eliphalet Read <eliphaletread@outlook.com>
2024-03-17 10:36:42 -0300

    Fixed deployment issue in Main

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import App from "./App.tsx";
 import "./assets/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // Temporarily removed React.StrictMode becasue of re-rendering issues
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
